21b3adb1c7b921e153d3d22711086584025da64d
adnanshibli <adnannajem.24.95@gmail.com>
2020-12-15 12:51:59 +0200

    is

---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE covidd; 
+CREATE TABLE covidd(
+    Country varchar (255),
+    TotalConfirmed varchar (255),
+    TotalDeaths varchar (255),
+    TotalRecovered varchar (255),
+    Date varchar (225)
+    );
